ec263ce22985bbafd18cf14e8021459715185a23
Nikita Tretyakov <tretyakov@mpip-mainz.mpg.de>
2015-03-13 12:15:44 +0100

    changed submitting structure

---
+++ send_gc_groove.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+Lx=$1
+Ly="1.6"
+Lz="16.4"
+Cx=$2
+Dx=$3
+Cy=${Ly}
+Dy=${Ly}
+Cz="2."
+Dz="0."
+den="33.40"
+lambda=$4
+nDims="2"
+NVT="0"
+eps="0.15"
+#Dx=$(( ${Lx}-${Cx} ))
+qsub -v den=${den},eps=${eps},lambda=${lambda},Lx=${Lx},Ly=${Ly},Lz=${Lz},Cx=${Cx},Cy=${Cy},Cz=${Cz},Dx=${Dx},Dy=${Dy},Dz=${Dz},nDims=${nDims},NVT=${NVT} submit_gc_groove.sh
---
+++ submit_gc_groove.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#$ -N GCgro_18.0
+# Send mail to me:
+#$ -M tretyakov@mpip-mainz.mpg.de
+#$ -q Single.q
+# Mail at beginning/end/on suspension:
+#$ -m be
+# The job is located in the current working directory:
+#$ -cwd
+
+#rm -rf /data/isilon/tretyakov/mf_lb/e${eps}_den${den}_L${Lx}-${Ly}-${Lz}_C${Cx}_D${Dx}/
+#mkdir /data/isilon/tretyakov/mf_lb/e${eps}_den${den}_L${Lx}-${Ly}-${Lz}_C${Cx}_D${Dx}/
+./gen_2014_11_26.out -path pckr160 -folder e${eps}_den${den}_L${Lx}-${Ly}-${Lz}_C${Cx}_D${Dx}/ \
+-den ${den} \
+-restart 1 \
+-lambda ${lambda} \
+-Lbox ${Lx} ${Ly} ${Lz} \
+-corr ${Cx} ${Cy} ${Cz} \
+-cav ${Dx} ${Dy} ${Dz} \
+-eps ${eps} \
+-nMol 2000 \
+-nDims ${nDims} \
+-NVT ${NVT}
